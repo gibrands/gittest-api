7f70a69e624b4040d2cf20c2fe0f019e3c495650
gibrands <ing.gibran.duran@gmail.com>
2019-10-30 16:10:32 -0700

    Cambios en config

--- config.js
+++ config.js
@@ -1,1 +1,6 @@
 console.log("GIT HUB Config file");
+const numeros = [1,3,4,5,1,23,4,2];
+for (let index = 0; index < numeros.length; index++) {
+    const element = numeros[index];
+    console.log(element);
+}
